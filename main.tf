7d3c447241453177bd4cbd49cca8f6a05e131af4
Arnold Filip <arnold.filip@technia.com>
2021-02-10 13:40:30 +0100

    Use azurerm backend

--- main.tf
+++ main.tf
@@ -5,7 +5,12 @@ terraform {
       version = ">= 2.26"
     }
   }
-  # backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "Lite3D"
+    storage_account_name = "litecloudvolumes"
+    container_name       = "tfstate"
+    key                  = "codelab.microsoft.tfstate"
+  }
 }
 
 provider "azurerm" {
